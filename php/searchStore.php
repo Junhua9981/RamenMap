e8bc6fd8c714938c3cf1512b2230e56861755f9e
fcebk17 <fcebk17@gmail.com>
2022-01-05 03:36:51 +0800

    更新search store

--- php/searchStore.php
+++ php/searchStore.php
@@ -2,7 +2,7 @@
     include "db_conn_ramen.php";
     $StoreName = $_REQUEST["StoreName"]; //前端需要同步
 
-    $query = ("SELECT storename, MRTStation, MRTRoute, Tel, BusinessHour, iframe FROM store WHERE StoreName LIKE '%" .$StoreName ."%' ");   
+    $query = ("SELECT StoreName,MRTStation, MRTRoute, Tel, BusinessHour, iframe FROM store WHERE StoreName LIKE '%" .$StoreName ."%' ");   
     $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
     $error = $stmt->execute(); //執行sql語法
     $result = $stmt->fetchAll();
