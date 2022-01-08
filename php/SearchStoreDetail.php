35ddde0e04bbe2a37930c7de8503e3f24d7af8d8
fcebk17 <fcebk17@gmail.com>
2022-01-09 00:40:24 +0800

    更正

--- php/SearchStoreDetail.php
+++ php/SearchStoreDetail.php
@@ -2,7 +2,7 @@
     include "db_conn_ramen.php";
     $StoreID = $_REQUEST["StoreID"]; //前端需要同步
 
-    $query = ("SELECT store.StoreID,store.StoreName, store.MRTStation, store.MRTRoute, Tel, BusinessHour, iframe, Address FROM store, location WHERE store.StoreID =? and location.StoreID = store.StoreID ");   
+    $query = ("SELECT location.StoreID,location.StoreName, location.MRTStation, location.MRTRoute, store.Tel, store.BusinessHour, store.iframe, location.Address FROM store, location WHERE store.StoreID =? and location.StoreID = store.StoreID ");   
     $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
     $error = $stmt->execute(array($StoreID)); //執行sql語法
     $result = $stmt->fetchAll();
