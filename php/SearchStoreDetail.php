a1aeff0b556c66660d586cf3da1b1a4cd44c33c3
fcebk17 <fcebk17@gmail.com>
2022-01-08 23:51:03 +0800

    通通拿去啦

---
+++ php/SearchStoreDetail.php
@@ -0,0 +1,12 @@
+<?php
+    include "db_conn_ramenmap.php";
+    $StoreID = $_REQUEST["StoreID"]; //前端需要同步
+
+    $query = ("SELECT store.StoreID,store.StoreName, store.MRTStation, store.MRTRoute, Tel, BusinessHour, iframe, Address FROM store, location WHERE store.StoreID =? and location.StoreID = store.StoreID ");   
+    $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
+    $error = $stmt->execute(array($StoreID)); //執行sql語法
+    $result = $stmt->fetchAll();
+
+    echo json_encode($result);
+  
+?>
