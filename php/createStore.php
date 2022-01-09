8e583bffd6af901dbe01ba9c9551637fa11b7e12
fcebk17 <fcebk17@gmail.com>
2022-01-09 21:40:12 +0800

    更正新增店家

--- php/createStore.php
+++ php/createStore.php
@@ -1,8 +1,8 @@
 <?php
-    include "db_conn_ramen.php";
+    include "db_conn_ramenmap.php";
     $StoreName = $_REQUEST["StoreName"];
     $Tel = $_REQUEST["Tel"];
-    $MRTStation = $_REQUEST["MRTStation"]; //前端需要同步
+    $MRTStation = $_REQUEST["MRTStation"]; 
     $MRTRoute = $_REQUEST["MRTRoute"];
     $BusinessHour = $_REQUEST["BusinessHour"];
     $Address = $_REQUEST["Address"];
@@ -12,21 +12,19 @@
     $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
     $error = $stmt->execute(array($StoreName, $MRTStation, $MRTRoute, $Tel, $BusinessHour)); //執行sql語法
 
-    $query = ("INSERT INTO location(MRTStation, MRTRoute, Address, StoreName) VALUES(?,?,?,?)");
-    $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
-    $error = $stmt->execute(array($MRTStation,$MRTRoute,$Address,$StoreName));
-    
-    $query = ("SELECT MRTStation FROM store WHERE StoreName = ?");
-    $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
+    $query = ("SELECT StoreID From store WHERE StoreName = ?");
+    $stmt = $db->prepare($query); //db為db_conn.php新建的連線物件 
     $error = $stmt->execute(array($StoreName));
-    $result = $stmt->fetchAll();
+    $result = $stmt->fetchAll();  //將所有搜尋結果存於result
+    //echo json_encode($result);
 
-    //echo json_encode($result[0][0]);
-    //echo json_encode($MRTStation);
-    if($result[0][0] == $MRTStation){
-        echo json_encode("success");
-    }
-    else{
-        echo json_encode("fail");
-    }
+    $StoreID = $result[0][0];
+
+    //echo json_encode($StoreID);
+    $query = ("INSERT INTO location(StoreID, StoreName, MRTStation, MRTRoute, Address) VALUES(?,?,?,?,?)");
+    $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
+    $error = $stmt->execute(array($StoreID,$StoreName, $MRTStation,$MRTRoute,$Address));
+
+    echo json_encode($StoreID);
+    
 ?>
