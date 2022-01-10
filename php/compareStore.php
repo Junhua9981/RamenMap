84523494baa0b7bc42e42d85885b64dab90b5c82
fcebk17 <fcebk17@gmail.com>
2022-01-10 23:09:37 +0800

    傳入一個店家ID 回傳拉麵平均價價錢低於該店家的所有店家名字與ID

---
+++ php/compareStore.php
@@ -0,0 +1,18 @@
+<?php
+    include "db_conn_ramenmap.php";
+    $StoreID = $_REQUEST["StoreID"];
+    
+    $query = ("SELECT store.StoreID, store.StoreName
+               FROM ramen,store
+               GROUP BY ramen.StoreID
+               HAVING AVG(Price) <= (SELECT AVG(Price)
+                                     FROM ramen
+                                     GROUP BY StoreID
+                                     HAVING StoreID = ?) AND store.StoreID = ramen.StoreID");
+    
+    $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
+    $error = $stmt->execute(array($StoreID)); //執行sql語法
+    $result = $stmt->fetchAll();   //將所有搜尋結果存於result
+    echo json_encode($result); //回傳json格式
+
+?>
