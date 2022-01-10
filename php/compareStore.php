1e9b12b254acb089c98044a43a02c5bb1f84d090
fcebk17 <fcebk17@gmail.com>
2022-01-11 00:02:14 +0800

    全部都拿去啦

--- php/compareStore.php
+++ php/compareStore.php
@@ -2,14 +2,13 @@
     include "db_conn_ramen.php";
     $StoreID = $_REQUEST["StoreID"];
     
-    $query = ("SELECT store.StoreID, store.StoreName
+    $query = ("SELECT store.StoreID, store.StoreName, store.MRTStation, store.MRTRoute, store.Tel, store.BusinessHour
                FROM ramen,store
                GROUP BY ramen.StoreID
                HAVING AVG(Price) <= (SELECT AVG(Price)
                                      FROM ramen
                                      GROUP BY StoreID
                                      HAVING StoreID = ?) AND store.StoreID = ramen.StoreID");
-    
     $stmt = $db->prepare($query);    //db為db_conn_sofware.php新建的連線物件 
     $error = $stmt->execute(array($StoreID)); //執行sql語法
     $result = $stmt->fetchAll();   //將所有搜尋結果存於result
